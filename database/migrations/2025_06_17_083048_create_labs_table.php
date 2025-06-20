c7d01b327c111cbba6c7457e518efb44f6ff734c
mizanwidias <mizanforwork@gmail.com>
2025-06-20 18:33:37 +0700

    fixed canvas

--- database/migrations/2025_06_17_083048_create_labs_table.php
+++ database/migrations/2025_06_17_083048_create_labs_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('labs', function (Blueprint $table) {
             $table->id();
-            $table->string('nama');
+            $table->string('nam');
             $table->text('deskripsi')->nullable();
             $table->string('author')->nullable();
             $table->timestamps();
